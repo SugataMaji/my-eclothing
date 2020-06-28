e4db454805308542acd6746b5cc1811a87701f6b
Sugata Maji <sugatamaji@Xcelpross-MacBook-Pro.local>
2020-06-28 13:03:59 +0530

    Update our app to nativate using withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+
 import Homepage from './pages/homepage/homepage.component';
 
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
-    <Homepage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route  path='/hats' component={HatsPage}/>
+      </Switch>
+    </div>
   );
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,29 +12,34 @@ class Directory extends React.Component{
             {
                 title: 'Hats',
                 imageUrl: 'https://images.unsplash.com/photo-1565839412225-98641b2d7b5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-                id: 1
+                id: 1,
+                linkUrl: 'hats'
             },
             {
                 title: 'Jackets',
                 imageUrl: 'https://images.unsplash.com/photo-1576993537667-c6d2386f90a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-                id: 2
+                id: 2,
+                linkUrl: ''
             },
             {
                 title: 'sneakers',
                 imageUrl: 'https://images.unsplash.com/photo-1549298916-f52d724204b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-                id: 3
+                id: 3,
+                linkUrl: ''
             },
             {
                 title: 'Womens',
                 imageUrl: 'https://images.unsplash.com/photo-1529903384028-929ae5dccdf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
                 size: 'large',
-                id: 4
+                id: 4,
+                linkUrl: ''
             },
             {
                 title: 'Mens',
                 imageUrl: 'https://images.unsplash.com/photo-1528977695568-bd5e5069eb61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
                 size: 'large',
-                id: 5
+                id: 5,
+                linkUrl: ''
             }
 
         ]
@@ -46,8 +51,8 @@ render(){
     return(
         <div className="directory-menu">
             {
-                this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                this.state.sections.map(({ id, ...otherSectionsProps}) => (
+                    <MenuItem key={id} {...otherSectionsProps}/>
                 ))
             }
         </div>
